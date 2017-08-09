10d0aed11d47456937ca37291cebd0f40a871357
Yeongjin Lee <yeongjinnn@gmail.com>
2017-08-09 09:12:57 +0900

    Add ErrorMessage Component test.

---
+++ src/components/form/ErrorMessage.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import ErrorMessage from './ErrorMessage';
+
+it('renders error', () => {
+  const fieldName = 'name';
+  const error = {
+    data: {
+      [fieldName]: ['error-1l', 'error-2'],
+    },
+  };
+  const wrapper = shallow(<ErrorMessage name={fieldName} error={error} />);
+  expect(wrapper.find('.error-message li').length).toEqual(2);
+});
