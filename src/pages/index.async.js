b022f2260e4980c8d55c5b3480c6ed689387390f
Yeongjin Lee <yeongjinnn@gmail.com>
2017-08-09 16:49:42 +0900

    Add Post async component.

--- src/pages/index.async.js
+++ src/pages/index.async.js
@@ -4,3 +4,4 @@ export const Home = asyncRoute(() => import('./Home'));
 export const Events = asyncRoute(() => import('./Events'));
 export const Event = asyncRoute(() => import('./Event'));
 export const Posts = asyncRoute(() => import('./Posts'));
+export const Post = asyncRoute(() => import('./Post'));
