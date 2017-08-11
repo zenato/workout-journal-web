e697e97f7c3c63a451dd0f420ed222ca65b86784
Yeongjin Lee <yeongjinnn@gmail.com>
2017-08-11 14:36:46 +0900

    Add ErrorMessage Component props type checker.

--- src/components/form/ErrorMessage.js
+++ src/components/form/ErrorMessage.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { getErrorMessages } from 'lib/api';
 
-const ErrorMessage = ({ error, name, context = 'data' }) => {
+const ErrorMessage = ({ error, name, context }) => {
   const errorMessages = getErrorMessages(error, name, context);
   return errorMessages ? (
     <ul className="error-message">
       {errorMessages.map((message, idx) => <li key={idx}>{message}</li>)}
     </ul>
   ) : null;
+};
+
+ErrorMessage.propTypes = {
+  error: PropTypes.object,
+  name: PropTypes.string.isRequired,
+  context: PropTypes.string,
+};
+
+ErrorMessage.defaultProps = {
+  context: 'data',
 };
 
 export default ErrorMessage;
