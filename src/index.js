dc9c6b4de0e85fa022f8f55af286fcb0c94b08b6
Yeongjin Lee <yeongjinnn@gmail.com>
2017-08-16 12:57:30 +0900

    Moved CSS files to styles dir.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import Root from './client/Root';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import './styles/index.css';
 
 const render = Component => {
   ReactDOM.render(
