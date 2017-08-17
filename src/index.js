f3d3a7e5d6c4c6ce1da6a0a72c22292374ec89af
Yeongjin Lee <yeongjinnn@gmail.com>
2017-08-17 09:30:21 +0900

    Move common style to index.scss

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import Root from './client/Root';
 import registerServiceWorker from './registerServiceWorker';
+import './index.scss';
 
 const render = Component => {
   ReactDOM.render(
