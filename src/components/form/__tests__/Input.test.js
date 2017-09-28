dd80adf7f5a2272482c530a8c60eee4b04c8ab5f
Yeongjin Lee <yeongjinnn@gmail.com>
2017-09-28 22:06:31 +0900

    Remove unused form component.

--- src/components/form/__tests__/Input.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Input from '../Input';
-
-it('renders error', () => {
-  const wrapper = shallow(<Input type="text"  value="input" />);
-  expect(wrapper).toMatchSnapshot();
-});
--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,4 +1,1 @@
-export { default as Input } from './Input';
-export { default as Select } from './Select';
 export { default as Button } from './Button';
-export { default as ErrorMessage } from './ErrorMessage';
