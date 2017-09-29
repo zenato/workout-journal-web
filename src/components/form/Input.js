9e3e8996b01a72dae639cabe26446e481117ab11
Yeongjin Lee <yeongjinnn@gmail.com>
2017-09-29 13:26:43 +0900

    Add date type format.

--- src/components/form/Input.js
+++ src/components/form/Input.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
+import { formatDate } from 'lib/date';
 import styles from './Input.scss';
 
 const cx = classNames.bind(styles);
@@ -9,7 +10,13 @@ const Input = ({ input, label, type, meta: { touched, error } }) => (
   <div className={cx('item')}>
     <label>{label}</label>
     <div className={cx('field')}>
-      <input {...input} placeholder={label} type={type} className={cx('form-control')}/>
+      <input
+        {...input}
+        value={type === 'date' ? formatDate(input.value) : input.value}
+        placeholder={label}
+        type={type}
+        className={cx('form-control')}
+      />
       {touched && error && <span className={cx('error')}>{error}</span>}
     </div>
   </div>
