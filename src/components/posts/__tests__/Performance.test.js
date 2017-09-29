3a85e92716305905279b3bec376c1f2e62b1931a
Yeongjin Lee <yeongjinnn@gmail.com>
2017-09-29 16:28:15 +0900

    Pass the test for performance component.

--- src/components/posts/__tests__/Performance.test.js
+++ src/components/posts/__tests__/Performance.test.js
@@ -12,7 +12,7 @@ const events = [
   },
 ];
 
-const item = {
+const values = {
   id: 1,
   name: 'test',
   value: 10,
@@ -22,102 +22,20 @@ const item = {
 
 it('renders without errors', () => {
   shallow(
-    <table>
-      <tbody>
-        <Performance
-          events={events}
-          idx={0}
-          item={item}
-          error={null}
-          onChange={() => {}}
-          onDelete={() => {}}
-        />
-      </tbody>
-    </table>
+    <Performance
+      key={0}
+      name={'performance[0]'}
+      events={events}
+      values={values}
+      onDelete={() => {}}
+    />
   );
-});
-
-it('simulate change form', () => {
-  let performance = null;
-
-  function onChange(i, p) {
-    performance = { ...performance, ...p };
-  }
-
-  const wrapper = mount(
-    <table>
-      <tbody>
-        <Performance
-          events={events}
-          idx={0}
-          item={item}
-          error={null}
-          onChange={onChange}
-          onDelete={() => {}}
-        />
-      </tbody>
-    </table>
-  );
-
-  // Changed input form
-  wrapper.find('select[name="event"]').simulate('change', {
-    target: {
-      name: 'event',
-      value: '1',
-    },
-  });
-  wrapper.find('input[name="value"]').simulate('change', {
-    target: {
-      name: 'value',
-      value: 10,
-    },
-  });
-  wrapper.find('input[name="set1"]').simulate('change', {
-    target: {
-      name: 'set1',
-      value: 1,
-    },
-  });
-  wrapper.find('input[name="set2"]').simulate('change', {
-    target: {
-      name: 'set2',
-      value: 2,
-    },
-  });
-  wrapper.find('input[name="set3"]').simulate('change', {
-    target: {
-      name: 'set3',
-      value: 3,
-    },
-  });
-  wrapper.find('input[name="set4"]').simulate('change', {
-    target: {
-      name: 'set4',
-      value: 4,
-    },
-  });
-  wrapper.find('input[name="set5"]').simulate('change', {
-    target: {
-      name: 'set5',
-      value: 5,
-    },
-  });
-
-  expect(performance).toEqual({
-    value: 10,
-    event: { id: '1', name: 'test', value: 10, unit: 'g', remark: 'test' },
-    set1: 1,
-    set2: 2,
-    set3: 3,
-    set4: 4,
-    set5: 5,
-  });
 });
 
 it('simulate delete click', () => {
   const onDelete = jest.fn();
 
-  const wrapper = mount(
+  const component = mount(
     <table>
       <tbody>
         <Performance
@@ -132,9 +50,7 @@ it('simulate delete click', () => {
     </table>
   );
 
-  wrapper.find('input[type="button"]').simulate('click', {
-    preventDefault: () => {},
-  });
+  component.find('input[type="button"]').simulate('click', { preventDefault () {} });
 
-  expect(onDelete).toBeCalledWith(1);
+  expect(onDelete).toBeCalled();
 });
