135ddfec17e1902df68631777c516a9ef65edec9
Yeongjin Lee <yeongjinnn@gmail.com>
2017-10-06 22:50:51 +0900

    Update Nav component test.

--- src/components/__tests__/Nav.test.js
+++ src/components/__tests__/Nav.test.js
@@ -2,7 +2,26 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Nav from '../Nav';
 
-it('renders without crashing', () => {
-  const wrapper = shallow(<Nav />);
-  expect(wrapper).toMatchSnapshot();
+const props = {
+  user: null,
+  onLogout: () => {},
+};
+
+it('Render without `user` prop.', () => {
+  const component = shallow(<Nav {...props} />);
+  expect(component).toMatchSnapshot();
+});
+
+it('Render with `user` prop.', () => {
+  const user = { username: 'username' };
+  const component = shallow(<Nav {...props} user={user} />);
+  expect(component).toMatchSnapshot();
+});
+
+it('Call `logout` prop when click logout link.', () => {
+  const onLogout = jest.fn();
+  const user = { username: 'username' };
+  const component = shallow(<Nav {...props} user={user} onLogout={onLogout} />);
+  component.find('aside a').simulate('click');
+  expect(onLogout).toBeCalled();
 });
