a69857a9a415ff4bec7f47e6811f8296f62f4046
Yeongjin Lee <yeongjinnn@gmail.com>
2017-10-07 17:54:19 +0900

    Add EventForm component unit test.

--- src/components/events/__tests__/EventForm.test.js
+++ src/components/events/__tests__/EventForm.test.js
@@ -2,15 +2,28 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import EventForm from '../EventForm';
 
-const props = {
-  handleSubmit: jest.fn(),
-  initialValues: {},
-  error: null,
-  onDelete: jest.fn(),
-  onMoveList: jest.fn(),
-};
+describe('EventForm', () => {
+  const props = {
+    handleSubmit: jest.fn(),
+    initialValues: null,
+    error: null,
+    onDelete: () => {},
+    onMoveList: () => {},
+  };
 
-it('Render without errors.', () => {
-  const component = shallow(<EventForm {...props} />);
-  expect(component).toMatchSnapshot();
+  it('should render error message if not empty `error` props.', () => {
+    const component = shallow(<EventForm {...props} error={['error']} />);
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should render delete button if not empty `initialValues` props.', () => {
+    const component = shallow(<EventForm {...props} initialValues={{ id: '1' }} />);
+    expect(component).toMatchSnapshot();
+  });
+
+  it('call `handleSubmit` when submitting form.', () => {
+    const component = shallow(<EventForm {...props} />);
+    component.find('form').simulate('submit');
+    expect(props.handleSubmit).toBeCalled();
+  });
 });
