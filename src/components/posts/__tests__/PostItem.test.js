1826a0024cbbdf4cb5f6a3c0ae0d99cad676ac37
Yeongjin Lee <yeongjinnn@gmail.com>
2017-10-08 09:25:51 +0900

    Update PostItem unit test.

--- src/components/posts/__tests__/PostItem.test.js
+++ src/components/posts/__tests__/PostItem.test.js
@@ -2,36 +2,31 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import PostItem from '../PostItem';
 
-const item = {
-  id: '1',
-  workoutDate: '2017-08-18T05:54:17.061Z',
-  remark: 'test',
-  performances: [
-    {
-      event: {
-        name: 'test',
-      },
+describe('PostItem', () => {
+  const props = {
+    item: {
+      id: '1',
+      workoutDate: '2017-08-18T05:54:17.061Z',
+      remark: 'test',
+      performances: [
+        {
+          event: {
+            name: 'test',
+          },
+        },
+      ],
     },
-  ],
-};
+    onDetail: jest.fn(),
+  };
 
-it('renders without error', () => {
-  shallow(
-    <PostItem
-      item={item}
-      onDetail={() => {}}
-    />
-  );
-});
+  it('should render without error.', () => {
+    const component = shallow(<PostItem {...props} />);
+    expect(component).toMatchSnapshot();
+  });
 
-it('simulate click link', () => {
-  let detailId = -1;
-  const wrapper = shallow(
-    <PostItem
-      item={item}
-      onDetail={(e, id) => detailId = id}
-    />
-  );
-  wrapper.find('a').simulate('click');
-  expect(detailId).toEqual('1');
+  it('should call `onDetail` if click link.', () => {
+    const component = shallow(<PostItem {...props} />);
+    component.find('a').simulate('click', { preventDefault: () => {} });
+    expect(props.onDetail.mock.calls[0][0]).toBe('1');
+  });
 });
