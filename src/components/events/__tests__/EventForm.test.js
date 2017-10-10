3d666619347a919cde2a901ea3d6ffd1b1217b06
Yeongjin Lee <yeongjinnn@gmail.com>
2017-10-10 10:35:23 +0900

    Add unit test to EventForm component.

--- src/components/events/__tests__/EventForm.test.js
+++ src/components/events/__tests__/EventForm.test.js
@@ -7,8 +7,8 @@ describe('EventForm', () => {
     handleSubmit: jest.fn(),
     initialValues: null,
     error: null,
-    onDelete: () => {},
-    onMoveList: () => {},
+    onDelete: jest.fn(),
+    onMoveList: jest.fn(),
   };
 
   it('should render error message if not empty `error` props.', () => {
@@ -21,9 +21,21 @@ describe('EventForm', () => {
     expect(component).toMatchSnapshot();
   });
 
-  it('call `handleSubmit` when submitting form.', () => {
+  it('should call `handleSubmit` when submitting form.', () => {
     const component = shallow(<EventForm {...props} />);
     component.find('form').simulate('submit');
     expect(props.handleSubmit).toBeCalled();
+  });
+
+  it('should passes `onMoveList` to list button component.', () => {
+    const component = shallow(<EventForm {...props} />);
+    const button = component.find('Button[value="List"]').get(0);
+    expect(button.props.onClick).toBe(props.onMoveList);
+  });
+
+  it('should passes `onDelete` to delete button component.', () => {
+    const component = shallow(<EventForm {...props} initialValues={{ id: '1' }} />);
+    const button = component.find('Button[value="Delete"]').get(0);
+    expect(button.props.onClick).toBe(props.onDelete);
   });
 });
