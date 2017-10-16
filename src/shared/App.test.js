79c54acd123f4177738652b2f1f1196862394ab3
Yeongjin Lee <yeongjinnn@gmail.com>
2017-10-16 11:11:37 +0900

    Update app test case.

--- src/shared/App.test.js
+++ src/shared/App.test.js
@@ -3,6 +3,9 @@ import { shallow } from 'enzyme';
 import App from './App';
 
 
-it('renders without crashing', () => {
-  shallow(<App />);
+describe('App', () => {
+  it('renders without crashing', () => {
+    const component = shallow(<App />);
+    expect(component).toMatchSnapshot();
+  });
 });
