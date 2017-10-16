a2a63b8436536fb531bfdcf5e731cee214988e93
Yeongjin Lee <yeongjinnn@gmail.com>
2017-10-16 17:54:18 +0900

    Remove input scss.

--- src/components/form/Input.js
+++ src/components/form/Input.js
@@ -1,28 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classNames from 'classnames/bind';
+import styled from 'styled-components';
 import { formatDate } from 'lib/date';
-import styles from './Input.scss';
 
-const cx = classNames.bind(styles);
+const Wrapper = styled.input`
+  display: block;
+  background-color: #fff;
+  color: #4a4a4a;
+  border: solid #d4d4d4 1px;
+  padding: 10px;
+  width: 100%;
+  text-align: ${({ type }) => (type === 'number' ? 'right' : 'left')};
+`;
+
+const Error = styled.span`
+  color: #f00;
+  font-size: 0.7rem;
+  padding: 5px;
+`;
 
 const Input = ({ input, label, type, meta: { touched, error } }) => (
   <div>
-    <input
+    <Wrapper
       {...input}
       value={type === 'date' ? formatDate(input.value) : input.value}
       placeholder={label}
       type={type}
-      className={cx('form-control')}
     />
-    {touched && error && <span className={cx('error')}>{error}</span>}
+    {touched && error && <Error>{error}</Error>}
   </div>
 );
 
 Input.propTypes = {
-  input: PropTypes.object.isRequired,
-  label: PropTypes.string,
   type: PropTypes.string.isRequired,
+  label: PropTypes.string,
+  input: PropTypes.object.isRequired,
   meta: PropTypes.shape({
     touched: PropTypes.bool,
     error: PropTypes.string,
