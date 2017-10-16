00f7f96d6841d938a4ba00ed97d7a81554b75676
Yeongjin Lee <yeongjinnn@gmail.com>
2017-10-16 17:28:05 +0900

    Change scss to styled component.

--- src/components/form/Button.js
+++ src/components/form/Button.js
@@ -1,11 +1,30 @@
 import React from 'react';
-import classNames from 'classnames/bind';
-import styles from './Button.scss';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-const cx = classNames.bind(styles);
+const Wrapper = styled.input`
+  background-color: ${({ color }) => color === 'primary' ? '#008ee0' : '#fff'};
+  color: ${({ color }) => color === 'primary' ? '#fff' : '#323232'};
+  padding: 10px;
+  border: solid #d4d4d4 1px;
+  cursor: pointer;
+  &:hover {
+    color: ${({ color }) => color === 'primary' ? '#fff' : '#161616'};
+    background-color: ${({ color }) => color === 'primary' ? '#00a4ff' : '#f6f6f6'} ;
+  }
+  &:active {
+    color: #161616;
+    background-color: #efefef;
+  }
+`;
 
-const Button = ({ type, className, ...props }) => (
-  <input {...props} type={type || 'button'} className={cx('btn', className)} />
+const Button = ({ type, color, ...props }) => (
+  <Wrapper {...props} type={type || 'button'} color={color} />
 );
+
+Button.propTypes = {
+  type: PropTypes.string,
+  color: PropTypes.string,
+};
 
 export default Button;
