415ca64bf4ad3b23c8c962ada4f0f270d656a143
Yeongjin Lee <yeongjinnn@gmail.com>
2017-10-31 16:55:16 +0900

    Fixed nav test case bugs.

--- src/components/__tests__/Nav.test.js
+++ src/components/__tests__/Nav.test.js
@@ -4,17 +4,17 @@ import Nav from '../Nav'
 
 describe('Nav', () => {
   const props = {
-    user: null,
+    loggedInfo: null,
     onLogout: jest.fn(),
   }
 
   it('should render logout button if not empty `user` prop.', () => {
-    const component = shallow(<Nav {...props} user={{ username: 'tester' }} />).dive()
+    const component = shallow(<Nav {...props} loggedInfo={{ username: 'tester' }} />).dive()
     expect(component).toMatchSnapshot()
   })
 
   it('call `logout` props when click logout link.', () => {
-    const component = shallow(<Nav {...props} user={{ username: 'tester' }} />).dive()
+    const component = shallow(<Nav {...props} loggedInfo={{ username: 'tester' }} />).dive()
     component.find('a').simulate('click')
     expect(props.onLogout).toBeCalled()
   })
