5a4a8e986623e3622513b97c1a358ffd808ef85c
Yeongjin Lee <yeongjinnn@gmail.com>
2017-10-31 10:21:11 +0900

    Remove deprecated takeEvery.

--- src/redux/sagas/events.js
+++ src/redux/sagas/events.js
@@ -1,5 +1,4 @@
-import { takeEvery } from 'redux-saga'
-import { fork, select, call, put } from 'redux-saga/effects'
+import { fork, select, call, put, takeEvery } from 'redux-saga/effects'
 import { REQUEST_FETCH_EVENTS, successFetchEvents, failureFetchEvents } from 'redux/modules/events'
 import * as api from 'lib/api'
 
@@ -17,12 +16,9 @@ function* fetchEvents({ payload: { query, done } }) {
 }
 
 function* create() {
-  yield* takeEvery(REQUEST_FETCH_EVENTS, fetchEvents)
+  yield takeEvery(REQUEST_FETCH_EVENTS, fetchEvents)
 }
 
 export default function* rootSaga() {
-  // Wait for sign-in
-  //yield take(SUCCESS_SIGN_IN);
-
   yield fork(create)
 }
