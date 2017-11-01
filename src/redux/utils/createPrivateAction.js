38727e27d77dec026d40b129ffbd12e2a374f0fd
Yeongjin Lee <yeongjinnn@gmail.com>
2017-11-01 10:57:46 +0900

    Remove unused private action lib.

--- src/redux/utils/createPrivateAction.js
+++
@@ -1,6 +0,0 @@
-import { createAction } from 'redux-actions'
-
-export default (type, action) => (...params) => (dispatch, getState) => {
-  const { accessToken } = getState()
-  return dispatch(createAction(type, action(accessToken))(...params))
-}
