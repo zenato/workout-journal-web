f15a3bea5a083f04fbd01261867dc9ef0e786416
Yeongjin Lee <yeongjinnn@gmail.com>
2017-11-01 20:14:59 +0900

    Redirect to login when emtpy access token.

--- src/shared/PrivateRoute.js
+++ src/shared/PrivateRoute.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { Redirect } from 'react-router'
 import { Route } from 'react-router-dom'
 
-const PrivateRoute = ({ component: Component, pending, loggedInfo, ...rest }) => (
-  <Route {...rest} render={props => (loggedInfo ? <Component {...props} /> : null)} />
+const PrivateRoute = ({ component: Component, pending, accessToken, loggedInfo, ...rest }) => (
+  <Route
+    {...rest}
+    render={props => {
+      if (!accessToken) {
+        return <Redirect to="/login" />
+      }
+      return loggedInfo ? <Component {...props} /> : null
+    }}
+  />
 )
 
 export default connect(state => ({
+  accessToken: state.users.accessToken,
   loggedInfo: state.users.loggedInfo,
 }))(PrivateRoute)
