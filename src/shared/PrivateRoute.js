90088412a4205aa32539ed8f504c565af43001ec
Yeongjin Lee <yeongjinnn@gmail.com>
2017-11-02 10:40:40 +0900

    Remove waiting for loggedInfo.

--- src/shared/PrivateRoute.js
+++ src/shared/PrivateRoute.js
@@ -3,19 +3,18 @@ import { connect } from 'react-redux'
 import { Redirect } from 'react-router'
 import { Route } from 'react-router-dom'
 
-const PrivateRoute = ({ component: Component, pending, accessToken, loggedInfo, ...rest }) => (
+const PrivateRoute = ({ component: Component, pending, accessToken, ...rest }) => (
   <Route
     {...rest}
     render={props => {
       if (!accessToken) {
         return <Redirect to="/signIn" />
       }
-      return loggedInfo ? <Component {...props} /> : null
+      return <Component {...props} />
     }}
   />
 )
 
 export default connect(state => ({
   accessToken: state.users.accessToken,
-  loggedInfo: state.users.loggedInfo,
 }))(PrivateRoute)
