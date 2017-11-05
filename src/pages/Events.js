1236fd5949014f756a5bec6cb948a9730a9b23c8
Yeongjin Lee <yeongjinnn@gmail.com>
2017-11-05 16:14:23 +0900

    Change pass param for fetch.

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -18,19 +18,21 @@ const EventSearchForm = reduxForm({
 
 class Events extends Component {
   componentWillMount() {
-    const { items, done } = this.props
+    const { location, items, done } = this.props
     if (!items) {
-      this.fetchData(this.props, done)
+      this.fetchData(location, done)
+    } else {
+      done()
     }
   }
 
-  componentWillReceiveProps(nextProps) {
-    if (hasChangedLocation(this.props.location, nextProps.location)) {
-      this.fetchData(nextProps)
+  componentWillReceiveProps({ location }) {
+    if (hasChangedLocation(this.props.location, location)) {
+      this.fetchData(location)
     }
   }
 
-  fetchData({ location }, done) {
+  fetchData(location, done) {
     const query = queryString.parse(location.search)
     return this.props.actions.fetchEvents({ query, done })
   }
