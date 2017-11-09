959613f8fa87d9c567e94a85a49336e1b2ab7519
Yeongjin Lee <yeongjinnn@gmail.com>
2017-11-09 13:22:03 +0900

    Remove accessToken props.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,20 +3,19 @@ import { connect } from 'react-redux'
 import { Helmet } from 'react-helmet'
 import { PAGE_TITLE } from 'config'
 
-const Home = ({ accessToken, loggedInfo }) => {
+const Home = ({ loggedInfo }) => {
   return (
     <div className="home">
       <Helmet>
         <title>{PAGE_TITLE}</title>
       </Helmet>
 
-      {accessToken && loggedInfo && <article>Welcome {loggedInfo.username}!</article>}
-      {!accessToken && <article>Required sign in.</article>}
+      {loggedInfo && <article>Welcome {loggedInfo.username}!</article>}
+      {!loggedInfo && <article>Required sign in.</article>}
     </div>
   )
 }
 
 export default connect(state => ({
-  accessToken: state.users.accessToken,
   loggedInfo: state.users.loggedInfo,
 }))(Home)
