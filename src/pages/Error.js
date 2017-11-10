5022225bcf66a4c6f1ea34f41ad076f569bf2ee6
Yeongjin Lee <yeongjinnn@gmail.com>
2017-11-10 23:43:00 +0900

    Check unauthorization error and move to sign-in page.

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -2,20 +2,38 @@ import _ from 'lodash'
 import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom'
 
+const unauthorization = error => {
+  const statusCode = _.get(error, 'response.status') || 500
+  return statusCode > 400 && statusCode < 500
+}
+
+const goSignIn = ({ push }) => push('/signIn')
+
 class Error extends Component {
+  state = {
+    initialized: true,
+  }
+
+  componentDidMount() {
+    const { error, history } = this.props
+    if (unauthorization(error)) {
+      goSignIn(history)
+    } else {
+      this.setState({ initialized: true })
+    }
+  }
+
   componentWillReceiveProps({ error }) {
-    const { history, error: prevError } = this.props
-    if (prevError !== error) {
-      const statusCode = _.get(error, 'response.status') || 500
-      if (statusCode > 400 && statusCode < 500) {
-        history.push('/signIn')
-      }
+    const { history, error: prev } = this.props
+    if (prev !== error && unauthorization(error)) {
+      goSignIn(history)
     }
   }
 
   render() {
     const { history } = this.props
-    return (
+    const { initialized } = this.state
+    return !initialized ? null : (
       <div>
         <div>Oops, An expected error seems to have occurred.</div>
         <div>
